e667ac58534507f9c615fda9fc7920a0c564d1e1
memoTux <romeo@mendezfuentes.net>
2024-09-18 11:27:26 -0600

    [config] add websocket to nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  nitro: {
+    experimental: {
+      websocket: true
+    }
+  }
 })
