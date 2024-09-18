e839c30de706cca0a2953b2e0d01d3b68684b2df
memoTux <romeo@mendezfuentes.net>
2024-09-18 11:26:31 -0600

    [pages] app add form with validation

--- app.vue
+++ app.vue
@@ -1,9 +1,49 @@
+<script setup lang="ts">
+import { z } from 'zod'
+import type { FormSubmitEvent } from '#ui/types'
+
+const schema = z.object({
+  message: z.string(),
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive({
+  message: undefined,
+})
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data)
+}
+</script>
+
 <template>
-  <UContainer class="border border-primary rounded-lg">
+  <UContainer class="flex flex-col space-y-8 h-full">
     <h1 class="text-4xl text-primary">Tux Chat</h1>
-    <section id="chat">
-      <form></form>
-    </section>
+    <UContainer
+      id="chat"
+      class="border border-primary rounded-lg p-8 h-full w-1/2"
+    >
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <UFormGroup
+          label="Message"
+          name="message"
+          required
+        >
+          <UInput
+            v-model="state.message"
+            placeholder="Type a message..."
+          />
+        </UFormGroup>
+        <UButton type="submit"> Submit </UButton>
+      </UForm>
+    </UContainer>
   </UContainer>
 </template>
 
