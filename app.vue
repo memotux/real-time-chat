da7b77fcda4594344551ec8932c8eb067913e85e
memoTux <romeo@mendezfuentes.net>
2024-09-18 11:02:28 -0600

    [pages] app initial layout

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UContainer class="prose">
+    <h1 class="text-4xl">Tux Chat</h1>
+    <section id="chat">
+      <form></form>
+    </section>
+  </UContainer>
 </template>
+
+<style>
+body {
+  width: 100dvw;
+  height: 100dvh;
+}
+#__nuxt {
+  width: 100%;
+  height: 100%;
+}
+</style>
