d91c2da70775b62ed62f4e8791c66209971dd0fc
memoTux <romeo@mendezfuentes.net>
2024-09-18 12:35:56 -0600

    [pages] app use websocket on form submit

--- app.vue
+++ app.vue
@@ -8,29 +8,48 @@ const schema = z.object({
 
 type Schema = z.output<typeof schema>
 
-const state = reactive({
-  message: undefined,
+interface State extends Schema {
+  connected: boolean
+  chat: Array<string>
+}
+
+const state = reactive<State>({
+  message: '',
+  connected: false,
+  chat: [],
 })
+let socket: WebSocket
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
   // Do something with data
   console.log(event.data)
+  socket.send(event.data.message)
+  state.message = ''
 }
 
 onMounted(() => {
   // Create WebSocket connection.
-  const socket = new WebSocket('ws://localhost:3000/chat')
+  socket = new WebSocket('ws://localhost:3000/chat')
 
   // Connection opened
-  socket.addEventListener('open', (event) => {
-    socket.send('Hello Server! ping')
-  })
+  socket.addEventListener('open', socketOnOpen)
 
   // Listen for messages
-  socket.addEventListener('message', (event) => {
-    console.log('Message from server ', event.data)
-  })
+  socket.addEventListener('message', socketOnMessage)
 })
+
+onUnmounted(() => {
+  socket.removeEventListener('open', socketOnOpen)
+  socket.removeEventListener('message', socketOnMessage)
+  socket.close()
+})
+
+function socketOnOpen() {
+  state.connected = true
+}
+function socketOnMessage(event: MessageEvent<string>) {
+  state.chat.push(event.data)
+}
 </script>
 
 <template>
@@ -38,17 +57,21 @@ onMounted(() => {
     <h1 class="text-4xl text-primary">Tux Chat</h1>
     <UContainer
       id="chat"
-      class="border border-primary rounded-lg p-8 h-full w-1/2"
+      class="border border-primary rounded-lg p-8 h-full w-1/2 flex flex-col justify-between"
     >
+      <UContainer class="w-full">
+        <pre>{{ state.chat }}</pre>
+      </UContainer>
       <UForm
         :schema="schema"
         :state="state"
-        class="space-y-4"
+        class="flex space-x-4 items-end w-full"
         @submit="onSubmit"
       >
         <UFormGroup
           label="Message"
           name="message"
+          class="w-full"
           required
         >
           <UInput
@@ -56,7 +79,12 @@ onMounted(() => {
             placeholder="Type a message..."
           />
         </UFormGroup>
-        <UButton type="submit"> Submit </UButton>
+        <UButton
+          type="submit"
+          :disable="!state.connected"
+        >
+          Submit
+        </UButton>
       </UForm>
     </UContainer>
   </UContainer>
