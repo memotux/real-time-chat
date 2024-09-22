f8dd6f22446b70f61bc9c94d4f2a062baa6cc27c
memoTux <romeo@mendezfuentes.net>
2024-09-22 10:41:22 -0600

    [server] routes/chat update errors logic

--- server/routes/chat.ts
+++ server/routes/chat.ts
@@ -1,17 +1,11 @@
 import type { Rooms } from "@/types";
+import { getRoomUser } from "../utils/chat";
 
 export default defineWebSocketHandler({
   async open(peer) {
-    const cookie = getCookie(peer.ctx, 'tuxchat') || '{"user": null, "room": null}'
+    const { room, user } = getRoomUser(peer.ctx, 'tuxchat')
 
-    const { user, room } = JSON.parse(cookie)
-
-    if (!user || !room) {
-      throw createError({
-        statusMessage: "User or Room not valid",
-        statusCode: 400
-      });
-    }
+    setCookie(peer.ctx, 'tuxchat', JSON.stringify({ room, user, auth: crypto.getRandomValues }))
 
     try {
       if (!await useStorage('db').hasItem('rooms.json')) {
@@ -26,33 +20,9 @@ export default defineWebSocketHandler({
         statusMessage: 'Unable to create DB'
       })
     }
-
+    let rooms: Rooms | null = null
     try {
-      const rooms = await useStorage('db').getItem<Rooms>('rooms.json')
-
-      if (rooms && !rooms[room]) {
-        // create room with empty array
-        rooms[room] = { users: [], messages: [] }
-        peer.send({ server: 'Room created.' })
-      }
-      if (rooms && !rooms[room].users.includes(user)) {
-        rooms[room].users.push(user)
-        peer.send({ server: 'User registered.' })
-      }
-
-      await useStorage('db')
-        .setItem(
-          'rooms.json',
-          JSON.stringify({ ...rooms })
-        )
-
-      if (rooms && rooms[room].messages.length > 0) {
-        // send messages history
-        peer.send({ history: rooms[room].messages })
-      }
-
-      peer.subscribe(room)
-      peer.send({ server: `Room ${room} open.` })
+      rooms = await useStorage('db').getItem('rooms.json')
     } catch (error) {
       console.error(error)
       throw createError({
@@ -61,56 +31,91 @@ export default defineWebSocketHandler({
       })
     }
 
+    let updateDB = false
+
+    if (rooms && !(room in rooms)) {
+      // create room with empty array
+      rooms[room] = { users: [], messages: [] }
+      peer.send({ server: 'Room created.' })
+      updateDB = true
+    }
+    if (rooms && !(user in rooms[room].users)) {
+      rooms[room].users.push(user)
+      peer.send({ server: 'User registered.' })
+      updateDB = true
+    }
+
+    if (updateDB) {
+      try {
+        await useStorage('db')
+          .setItem(
+            'rooms.json',
+            JSON.stringify({ ...rooms })
+          )
+      } catch (error) {
+        console.log(error);
+        throw createError({
+          statusCode: 500,
+          statusMessage: 'Error updating DB.'
+        })
+      }
+    }
+
+    if (rooms && rooms[room].messages.length > 0) {
+      // send messages history
+      peer.send({ history: rooms[room].messages })
+    }
+
+    peer.subscribe(room)
+    peer.send({ server: `Room "${room}" open.` })
+
     console.log("[ws] open", peer);
   },
 
   async message(peer, message) {
     // console.log("[ws] message", peer, message);
-    const cookie = getCookie(peer.ctx, 'tuxchat') || '{"user": null, "room": null}'
-    const { user, room } = JSON.parse(cookie)
+    const { user, room } = getRoomUser(peer.ctx, 'tuxchat')
 
-    if (!user || !room) {
-      throw createError({
-        statusMessage: "User or Room not valid",
-        statusCode: 400
-      });
-    }
-
+    let rooms: Rooms | null = null
     try {
-      const rooms = await useStorage<Rooms>('db').getItem('rooms.json')
-
-      if (!rooms) {
-        throw createError({
-          statusCode: 500,
-          statusMessage: 'Unable to get Rooms'
-        })
-      }
-
-      if (Object.keys(rooms).includes(room)) {
-        const newMessage = {
-          message: message.text(),
-          user,
-          id: crypto.randomUUID()
-        }
-        rooms[room].messages.push(newMessage)
-        peer.publish(room, { data: newMessage })
-        peer.send({ data: newMessage })
-        await useStorage('db').setItem('rooms.json', JSON.stringify(rooms))
-      } else {
-        throw createError({
-          statusCode: 403,
-          statusMessage: 'Room not open.'
-        })
-      }
+      rooms = await useStorage('db').getItem('rooms.json')
     } catch (error) {
       console.error(error);
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'Can not get Rooms.'
+      })
+    }
+
+    if (!rooms) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'Rooms not exist.'
+      })
+    }
+
+    if (room in rooms) {
+      const newMessage = {
+        message: message.text(),
+        user,
+        id: crypto.randomUUID()
+      }
+      rooms[room].messages.push(newMessage)
+      peer.publish(room, { data: newMessage })
+      peer.send({ data: newMessage })
+      await useStorage('db').setItem('rooms.json', JSON.stringify(rooms))
+    } else {
+      throw createError({
+        statusCode: 403,
+        statusMessage: 'Room not exist.'
+      })
     }
   },
 
   close(peer, event) {
-    const cookie = getCookie(peer.ctx, 'tuxchat') || '{"user": null, "room": null}'
-    const { room } = JSON.parse(cookie)
-    setCookie(peer.ctx, 'tuxchat', JSON.stringify(null))
+    const { room } = getRoomUser(peer.ctx, 'tuxchat')
+
+    // setCookie(peer.ctx, 'tuxchat', JSON.stringify(null))
     peer.unsubscribe(room)
     console.log("[ws] close", peer, event);
   },
