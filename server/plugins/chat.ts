ba6f913b72fd36eb039755111ab1ce7f32649fbc
memoTux <romeo@mendezfuentes.net>
2024-10-10 14:56:53 -0600

    [server] plugins chat handle error creating DBs

--- server/plugins/chat.ts
+++ server/plugins/chat.ts
@@ -1,8 +1,21 @@
 export default defineNitroPlugin(async () => {
   try {
-    await createTokensDB()
-    await createRoomsDB()
+    const tokens = await createTokensDB()
+    const rooms = await createRoomsDB()
+
+    if (!tokens || !rooms) {
+      throw createError({
+        statusCode: 500,
+        statusMessage: 'Unable to create DB during init.',
+        data: { tokens, rooms }
+      })
+    }
   } catch (error) {
     console.error(error);
+
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Error while initializing DBs.'
+    })
   }
 })
