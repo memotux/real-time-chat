0454ab41e8bca3904a6edfcde469e34f6b4ff195
memoTux <romeo@mendezfuentes.net>
2024-10-13 10:57:12 -0600

    [layers] auth use cases functions to server utils

--- layers/auth/nuxt.config.ts
+++ layers/auth/nuxt.config.ts
@@ -1,12 +1,1 @@
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-
-const currentDir = dirname(fileURLToPath(import.meta.url))
-
-export default defineNuxtConfig({
-  nitro: {
-    imports: {
-      dirs: [join(currentDir, './server/auth/*')]
-    }
-  }
-})
+export default defineNuxtConfig({})
