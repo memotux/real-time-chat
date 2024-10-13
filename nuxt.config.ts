72580a60f6af1eca0e51668a7b172873bdf9f9d9
memoTux <romeo@mendezfuentes.net>
2024-10-13 16:36:12 -0600

    [app] nuxt.config activate compatibilityVersion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4
+  },
   build: {
     transpile: ['jsonwebtoken']
   },
