fec4ca660fd9a0ed1e028f8159685375de5803e8
memoTux <romeo@mendezfuentes.net>
2024-10-13 20:23:42 -0600

    [layers] auth migrate config

--- layers/auth/nuxt.config.ts
+++ layers/auth/nuxt.config.ts
@@ -1,1 +1,32 @@
-export default defineNuxtConfig({})
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  build: {
+    transpile: ['jsonwebtoken']
+  },
+  modules: ['@sidebase/nuxt-auth'],
+  runtimeConfig: {
+    authSecret: ''
+  },
+  auth: {
+    provider: {
+      type: 'local',
+      endpoints: {
+        signIn: { path: '/login', method: 'post' },
+        signOut: { path: '/logout', method: 'post' },
+        getSession: { path: '/session', method: 'get' },
+      },
+      token: {
+        signInResponseTokenPointer: '/token/accessToken'
+      },
+      pages: {
+        login: '/'
+      },
+      session: {
+        dataType: {
+          room: 'string',
+          user: 'string',
+        },
+      },
+    }
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,35 +5,7 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4
   },
-  build: {
-    transpile: ['jsonwebtoken']
-  },
-  modules: ['@nuxt/ui', '@sidebase/nuxt-auth'],
-  runtimeConfig: {
-    authSecret: ''
-  },
-  auth: {
-    provider: {
-      type: 'local',
-      endpoints: {
-        signIn: { path: '/login', method: 'post' },
-        signOut: { path: '/logout', method: 'post' },
-        getSession: { path: '/session', method: 'get' },
-      },
-      token: {
-        signInResponseTokenPointer: '/token/accessToken'
-      },
-      pages: {
-        login: '/'
-      },
-      session: {
-        dataType: {
-          room: 'string',
-          user: 'string',
-        },
-      },
-    }
-  },
+  modules: ['@nuxt/ui'],
   nitro: {
     experimental: {
       websocket: true
